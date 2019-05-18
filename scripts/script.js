9cc05f368f3407bd15f81780b9268f2c0b108a56
laytommy <lay.thompsonn@gmail.com>
2019-05-18 15:48:41 -0400

    rework to script, made Question One append/html instead of leaving it as a fixed element..., did some testing to all choices to make sure interactive quiz is functional and smooth scrolling properly

--- scripts/script.js
+++ scripts/script.js
@@ -130,6 +130,9 @@ const foodExpensive = [];
 const activityChillin = [];
 const activityDate = [];
 
+let foodResult;
+let activityResult;
+
 // Global function created for foodAfford. (This function is created so we can grab the foodAfford's new array and generate a random index value so it can be returned to the user.)
 function randomIndex(foodAfford) {
 	const index = Math.floor(Math.random() * foodAfford.length);
@@ -150,79 +153,97 @@ function randomIndex(activityDate) {
 	return activityDate[index]
 }
 
-// const results = function (foodResult) {
-// 	$(`.results`).html(
-// 		`<div class="wrapper main">
-// 			<div class="resultsContent">
-// 				<h2>Here You Go!</h2>
-// 				<p>Looks like it's going to be...</p>
-// 				<p class="resultText">${foodResult}</p>
-// 			</div>
-// 		</div>`
-// 	);
-// }
-
-function displayResult(foodResult) {
+function displayResult(choice) {
 	$(`.results`).html(
 		`<div class="wrapper main">
 			<div class="resultsContent">
 				<h2>Wooo Hoo!</h2>
-				<p>Looks like you're going for:</p>
-				<h3 class ="resultText">"${foodResult}"</h3>
+				<p>Looks like you're going to:</p>
+				<h3 class ="resultText">"${choice}"</h3>
+				<a href = "#hero">
+					<button class="resetButton">Poke to Reset!</button>
+				</a
 			</div>
 		</div>`
-	)
+	)``
 };
 
 $(document).ready(function() {
 
-	// This set of functions are set to append the next set of choices based on USER selection.
-	$(`.secondButton`).on(`click`, function(event) {
+	// This set of functions are set to make Question One appear on click from the Start button. 
+	$(`.heroButton`).on(`click`, function (event) {
 		event.preventDefault();
 
-		if ($(`#food:checked`).val()) {
+		$(`.heroButton`).attr(`disabled`, true);
+		$(`html, body`).animate ({
+			scrollTop: $(`#primQuestion`).offset().top
+		}, 1000);
+
+		$(`.questionsOne`).html (
+		`<h2>Choose Your Path!</h2>
+			<p>You only get one try to discourage additional indecisiveness!</p>
+		<div class="inputLabel">
+			<input id="food" name="foodActivity" type="radio" value="food" required>
+			<label for="food">Food</label>
+			<input id="activity" name="foodActivity" type="radio" value="activity" required>
+			<label for="activity">Activity</label>
+		</div>
+		<input id="firstSubmit" class="firstSubmit" name="" type="submit" value="Next!" required>`
+		);
+
+		// This set of functions are set to append the next set of choices based on USER selection from Question One.
+		$(`.firstSubmit`).on(`click`, function (event) {
+			event.preventDefault();
 
 			// Prevent the USER from submiting more than once on submit, after selecting FOOD/ ACTIVITY.
-			$(`.secondButton`).attr(`disabled`, true);
+			$(`.firstSubmit`).attr(`disabled`, true);
+			$(`html, body`).animate({
+				scrollTop: $(`#subQuestion`).offset().top
+			}, 1000);
 
-			$(`.questionsTwo`).html(
-			`<h2>Hurry! I'm Hungry!</h2>
-			<div class="flexAlign">
-				<input id="affordable" name="affordableTreat" type="radio" value="affordable">
-				<label for="affordable">Affordable. Yuss!</label>
-				<input id="expensive" name="affordableTreat" type="radio" value="expensive">
-				<label for="expensive">Treat Yourself! Duh!</label>
-			</div>
-				<input id="subSubmit" class="subSubmit" name="" type="submit" value="Next!">`
-			);
-			
-		} else if ($(`#activity:checked`).val()) {
+			if ($(`#food:checked`).val()) {
 
-			$(`.secondButton`).attr(`disabled`, true);
+				$(`.questionsTwo`).html(
+				`<h2>Hurry! I'm Hungry!</h2>
+					<p>Choose another!</p>
+				<div class="flexAlign">
+					<input id="affordable" name="affordableTreat" type="radio" value="affordable" required>
+					<label for="affordable">Affordable. Yuss!</label>
+					<input id="expensive" name="affordableTreat" type="radio" value="expensive" required>
+					<label for="expensive">Treat Yourself! Duh!</label>
+				</div>
+					<input id="subSubmit" class="subSubmit" name="" type="submit" value="Next!">`
+				);
 
-			$(`.questionsTwo`).html(
-			`<h2>Sweet! Let's go!</h2>
-			<div class="flexAlign">
-				<input id="chillin" name="chillinDate" type="radio" value="chillin">
-				<label for="chillin">Chillin' Like A Villian?</label>
-				<input id="date" name="chillinDate" type="radio" value="date">
-				<label for="date">Date? Ouuu.</label>
-			</div>
-				<input id="subSubmit" class="subSubmit" name="" type="submit" value="Next!">`
-			);
-		}
+			} else if ($(`#activity:checked`).val()) {
+
+				$(`.questionsTwo`).html(
+					`<h2>Sweet! Let's go!</h2>
+					<p>Almost there!</p>
+					<div class="flexAlign">
+						<input id="chillin" name="chillinDate" type="radio" value="chillin" required>
+						<label for="chillin">Chillin' Like A Villian?</label>
+						<input id="date" name="chillinDate" type="radio" value="date" required>
+						<label for="date">Date? Ouuu.</label>
+					</div>
+						<input id="subSubmit" class="subSubmit" name="" type="submit" value="Next!">`
+				);
+			}
+		})
 	})
 
 	// This set of functions are set to generate a random index value from the arrays, based on USER selection.
 	$(`form`).on(`submit`, function(event) {
 		// Set a preventDefault behaviour for submit buttons.
 		event.preventDefault();
+		// Prevent the USER from submiting more than once on submit for Questions after selecting FOOD/ AFFORDABLE.
+		$(`.subSubmit`).attr(`disabled`, true);
+		$(`html, body`).animate({
+			scrollTop: $(`#results`).offset().top
+		}, 1000);
 
 		// IF, FOOD && AFFORDABLE are checked. Do the following...
 		if ($(`#food:checked`).val() && $(`#affordable:checked`).val()) {
-
-			// Prevent the USER from submiting more than once on submit for Questions after selecting FOOD/ AFFORDABLE.
-			$(`.subSubmit`).attr(`disabled`, true);
 
 			// RUN a FOR loop-de-loop for the FOOD list...
 			for (let i = 0; i < food.length; i++) {
@@ -234,16 +255,20 @@ $(document).ready(function() {
 			}
 
 			// Create a variable and make sure it holds onto whatever FUNCTION it produces from randomIndex(foodAfford).
-			const foodResult = randomIndex(foodAfford);
+			foodResult = randomIndex(foodAfford);
 
 			// DISPLAY/ RETURN the random results from the array to: (div/headings/paragraph etc.) // NOTE: Log console.log here to do tests/ debug
 			console.log(foodResult);
-			displayResult (foodResult);
-		
+			displayResult(foodResult);
+
+			$(`.resetButton`).on(`click`, () => {
+				$(`form`).trigger(`reset`);
+				$(`.firstSubmit`).attr(`disabled`, false);
+				// $(`form`).trigger(`reset`).remove();
+			})
+			
 		// ELSE IF, FOOD && EXPENSIVE are checked. Do the following...
 		} else if ($(`#food:checked`).val() && $(`#expensive:checked`).val())	{
-
-			$(`.subSubmit`).attr(`disabled`, true);
 
 			for (let i = 0; i < food.length; i++) {
 				if (food[i].price === `expensive`) {
@@ -251,15 +276,13 @@ $(document).ready(function() {
 				}
 			}
 
-			const foodResult = randomIndex(foodExpensive);
-			// console.log(foodResult);
-			// results(foodResult);
+			foodResult = randomIndex(foodExpensive);
+			console.log(foodResult);
+			displayResult(foodResult);
 		}
 
 		// IF, ACTIVITY && CHILLIN are checked. Do the following...
 		if ($(`#activity:checked`).val() && $(`#chillin:checked`).val()) {
-
-			$(`.subSubmit`).attr(`disabled`, true);
 
 			for (let i = 0; i < activity.length; i++) {
 				if (activity[i].type === `chillin`) {
@@ -267,14 +290,12 @@ $(document).ready(function() {
 				}
 			}
 
-			const activityResult = randomIndex(activityChillin);
-			// console.log(activityResult);
-			// results(activityResult);
+			activityResult = randomIndex(activityChillin);
+			console.log(activityResult);
+			displayResult(activityResult);
 
 			// ELSE IF, ACTIVITY && DATE are checked. Do the following...
 		} else if ($(`#activity:checked`).val() && $(`#date:checked`).val()) {
-
-			$(`.subSubmit`).attr(`disabled`, true);
 
 			for (let i = 0; i < activity.length; i++) {
 				if (activity[i].type === `date`) {
@@ -282,9 +303,10 @@ $(document).ready(function() {
 				}
 			}
 
-			const activityResult = randomIndex(activityDate);
-			// console.log(activityResult);
-			// results(activityResult);
+			activityResult = randomIndex(activityDate);
+			console.log(activityResult);
+			displayResult(activityResult);
 		}
 	})
 })
+
